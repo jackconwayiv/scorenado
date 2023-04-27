33139de2ee55f9eedb2fcf50508181e842aa066d
jackconwayiv <jackconwayiv@gmail.com>
2023-04-27 13:07:37 -0700

    game endpoint serves game object

--- datastructure.js
+++ datastructure.js
@@ -1,6 +1,6 @@
 const game = {
   id: 1,
-  userId: 1, //foreign key
+  // userId: 1, //foreign key
   playerName1,
   playerName2,
   playerName3,
@@ -9,42 +9,54 @@ const game = {
   playerName6,
   playerName7,
   playerName8,
+  createdAt,
+  updatedAt,
   templateId: 1, //foreign key //does this appear or get replaced by object
   template: {
     id: 1,
     name: "game",
     scoredByRounds: false,
     lowScoreWins: false,
+    createdAt,
+    updatedAt,
     categories: [
       {
         id: 1,
         name: "category",
         description: "how you earn points for this",
-        templateId: 1,
         isScored: true,
         isManualTotal: false,
-        scores: {
-          id: 1,
-          categoryId, //fk
-          gameId, //fk
-          value1,
-          value2,
-          value3,
-          value4,
-          value5,
-          value6,
-          value7,
-          value8,
-        },
+        createdAt,
+        updatedAt,
+        templateId: 1,
+        scores: [
+          {
+            id: 1,
+            categoryId, //fk
+            gameId, //fk
+            value1,
+            value2,
+            value3,
+            value4,
+            value5,
+            value6,
+            value7,
+            value8,
+          },
+        ],
       },
       {
         id: 2,
         name: "category",
         description: "how you earn points for this",
-        templateId: 1,
         isScored: true,
         manualTotal: false,
+        createdAt,
+        updatedAt,
+        templateId: 1,
+        scores: [],
       },
     ],
   },
+  scores: [],
 };
--- server/app.ts
+++ server/app.ts
@@ -1,31 +1,45 @@
-import express from "express";
-import path from "path";
-
+import express, { Request, Response } from "express";
+import { Game } from "../db";
+const morgan = require("morgan");
 const app = express();
 
-// const morgan = require("morgan");
-
-// logging middleware
-// app.use(morgan("dev"));
-
-// body parsing middleware
+app.use(morgan("dev"));
 app.use(express.json());
 
 // Configure Express to use EJS
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 
 // auth and api routes
-app.use("/auth", require("./auth"));
-app.use("/api", require("./api"));
-
-// define a route handler for the default home page
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "..", "public/index.html")),
-);
+// app.use("/auth", require("./auth"));
+// app.use("/api", require("./api"));
 
 // static file-serving middleware
-app.use(express.static(path.join(__dirname, "..", "public")));
+// app.use(express.static(path.join(__dirname, "..", "public")));
+
+//fetch a full game object -- is there a way to exclude the scores associated with the game, and only have it associated with the category?
+//i.e. will i run into trouble when there are multiple games with the same template and categories? will it aggressively load all scores for that category?
+app.get("/api/games/:id", async (req: Request, res: Response) => {
+  try {
+    const gameId: number = parseInt(req.params.id);
+    console.log("gameId is ", gameId);
+    const myGame = await Game.findByPk(gameId, {
+      // include: { model: Template, as: "template" },
+      include: { all: true, nested: true },
+    });
+    console.log("myGame is ", myGame);
+    res.send(myGame);
+  } catch (error: unknown) {
+    console.error("Sorry, we encountered an error: ", error);
+  }
+});
+
+// // define a route handler for the default home page
+// app.get(
+//   "/",
+//   (error: unknown, req: Request, res: Response, next: NextFunction) =>
+//     res.send("base uri"),
+// );
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
 // app.use((req, res, next) => {
@@ -39,9 +53,9 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 // });
 
 // sends index.html
-app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public/index.html"));
-});
+// app.use("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "public/index.html"));
+// });
 
 // error handling endware
 // app.use((err, req, res, next) => {
