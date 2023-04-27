00e223b22f5c2f0e82590f36f052321d2dec38f1
jackconwayiv <jackconwayiv@gmail.com>
2023-04-26 19:23:02 -0700

    ignoring dist

--- dist/db/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Sequelize = require("sequelize");
-const config = {
-    logging: false,
-};
-const db = new Sequelize(`postgres://localhost:5432/scorenado`, config);
-exports.default = db;
