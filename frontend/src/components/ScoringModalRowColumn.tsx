5ff5c4e10c4b324b065baf96b5cfa0bbbda1cae9
jackconwayiv <jackconwayiv@gmail.com>
2023-05-04 06:28:43 -0700

    plus and minus buttons in score modal input cells

--- frontend/src/components/ScoringModalRowColumn.tsx
+++ frontend/src/components/ScoringModalRowColumn.tsx
@@ -1,4 +1,4 @@
-import { Input } from "@chakra-ui/react";
+import { Button, Center, Input } from "@chakra-ui/react";
 
 interface ScoringModalRowColumnProps {
   val: number | null;
@@ -12,13 +12,39 @@ const ScoringModalRowColumn = ({
   rewriteScoresArray,
 }: ScoringModalRowColumnProps) => {
   return (
-    <Input
-      value={val !== null ? val : ""}
-      maxW={"35px"}
-      tabIndex={index + 1}
-      p={"5px"}
-      onChange={(e) => rewriteScoresArray(index, e.target.value)}
-    ></Input>
+    <Center>
+      <Button
+        minH="20px"
+        maxH="30px"
+        minW="20px"
+        maxW="30px"
+        onClick={() =>
+          rewriteScoresArray(index, val !== null ? (val - 1).toString() : "0")
+        }
+      >
+        -
+      </Button>
+      <Input
+        value={val !== null ? val : ""}
+        minW="40px"
+        maxW="50px"
+        textAlign="center"
+        tabIndex={index + 1}
+        p={"5px"}
+        onChange={(e) => rewriteScoresArray(index, e.target.value)}
+      ></Input>
+      <Button
+        minH="20px"
+        maxH="30px"
+        minW="20px"
+        maxW="30px"
+        onClick={() =>
+          rewriteScoresArray(index, val !== null ? (val + 1).toString() : "1")
+        }
+      >
+        +
+      </Button>
+    </Center>
   );
 };
 
